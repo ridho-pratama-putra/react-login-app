2b824a6797c40bc0de334a1530bb1091b874d2e3
ridho <ridhopratamavd@gmail.com>
2021-11-05 22:47:35 +0700

    save JWT

--- src/UniversalComponents/Home.js
+++ src/UniversalComponents/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Cookies from 'js-cookie';
+
 const Home = () => {
     if (Cookies.get('accessToken')) {
         localStorage.setItem('accessToken', Cookies.get('accessToken'))
         localStorage.setItem('refreshToken', Cookies.get('refreshToken'))
+        Cookies.remove('refreshToken')
+        Cookies.remove('accessToken')
     }
     return (
         <div>
