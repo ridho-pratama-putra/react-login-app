d8347e9bcc5f215cb7c09694e42fb3c2e5853eb6
ridho <ridhopratamavd@gmail.com>
2021-11-17 23:46:57 +0700

    add video in protected route

--- src/Page/Contents.js
+++ src/Page/Contents.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import VideoJS from '../UniversalComponents/VideoJS'
 
-function Contents(props) {
+function Contents() {
+
+    const playerRef = React.useRef(null);
+
+    const videoJsOptions = { // lookup the options in the docs for more options
+        autoplay: true,
+        controls: true,
+        responsive: "vjs-layout-medium",
+        fluid: true,
+        sources: [{
+            src: './Ello.mp4',
+            type: 'video/mp4'
+        }]
+    }
+
+    const handlePlayerReady = (player) => {
+        playerRef.current = player;
+
+        // you can handle player events here
+        player.on('waiting', () => {
+            console.log('player is waiting');
+        });
+
+        player.on('dispose', () => {
+            console.log('player will dispose');
+        });
+    };
     return (
-        <div>THIS IS CONTENTS</div>
+        <div>THIS IS CONTENTS
+            <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
     );
 }
 
