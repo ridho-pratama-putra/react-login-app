ba8f96db61034149d0cfe3a8c905d788410a6bbd
ridho <ridhopratamavd@gmail.com>
2021-12-04 10:17:07 +0700

    rename router endpoint

--- src/Api/User.js
+++ src/Api/User.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import instance from './Interceptor'
 const LOGIN_URL = 'http://localhost/auth';
-const REGISTER_URL = 'http://localhost/register';
-const LOGOUT_URL = 'http://localhost/logout';
+const REGISTER_URL = 'http://localhost/user';
 const FETCH_INTERNAL_ACCOUNT = 'http://localhost/internal-account';
 const CONTENT_URL = 'http://localhost/';
 
 export const getContent = () => instance.get(CONTENT_URL, {timeout: 5000});
 export const submitLogin = (formValue) => axios.post(LOGIN_URL, formValue, {timeout: 5000});
 export const submitRegister = (formValue) => axios.post(REGISTER_URL, formValue, {timeout: 5000});
-export const doLogout = (data) => axios.delete(LOGOUT_URL, data);
 export const fethInternalAccount = (data) => axios.post(FETCH_INTERNAL_ACCOUNT, data, {timeout: 5000});
