5ed37ec9ddcc24fe5fcd4ff2d69b4eb2fcbfece9
ridho <ridho.putra@btpn.com>
2021-12-05 21:36:48 +0700

    remove interceptor because already handled by nginx to check jwt validity

--- src/Api/User.js
+++ src/Api/User.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
-import instance from './Interceptor'
 const LOGIN_URL = 'http://localhost/auth';
 const REGISTER_URL = 'http://localhost/user';
 const FETCH_INTERNAL_ACCOUNT = 'http://localhost/internal-account';
-const CONTENT_URL = 'http://localhost/';
+const CONTENT_URL = 'http://localhost/contents';
 
-export const getContent = () => instance.get(CONTENT_URL, {timeout: 5000});
+export const getContent = () => axios.get(CONTENT_URL, {
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    }
+});
 export const submitLogin = (formValue) => axios.post(LOGIN_URL, formValue, {timeout: 5000});
 export const submitRegister = (formValue) => axios.post(REGISTER_URL, formValue, {timeout: 5000});
 export const fethInternalAccount = (data) => axios.post(FETCH_INTERNAL_ACCOUNT, data, {timeout: 5000});
